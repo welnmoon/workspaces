89a53ae55af9b4c89f3197b7c23169af93114edf
Nursultan Elshatuly <sultanelshatuly@gmail.com>
2025-11-11 20:32:02 +0500

    fix(sidebar): sidebar rail add, adaptive fix

--- components/sidebar/dynamic/dashboard-sidebar-dynamic.tsx
+++ components/sidebar/dynamic/dashboard-sidebar-dynamic.tsx
@@ -4,18 +4,22 @@ import {
   SidebarFooter,
   SidebarGroup,
   SidebarHeader,
+  SidebarRail,
 } from '@/components/ui/sidebar';
 
 export function DashboardSidebarDynamic() {
   return (
-    <Sidebar>
-      <SidebarHeader />
-      <SidebarContent>
-        HelloHelloHelloHelloHelloHe
-        <SidebarGroup />
-        <SidebarGroup />
-      </SidebarContent>
-      <SidebarFooter />
-    </Sidebar>
+    <>
+      <Sidebar className="z-40 w-5/6 md:w-1/2">
+        <SidebarHeader />
+        <SidebarContent>
+          HelloHelloHelloHelloHelloHe
+          <SidebarGroup />
+          <SidebarGroup />
+        </SidebarContent>
+        <SidebarFooter />
+      </Sidebar>
+      <SidebarRail side="left" railWidth={48} peekPx={12} openGapPx={8} />
+    </>
   );
 }
