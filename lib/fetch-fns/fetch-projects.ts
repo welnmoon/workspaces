b10a276d3628d76a91091551b2fd0e75fcd6fdac
Nursultan Elshatuly <sultanelshatuly@gmail.com>
2025-11-19 20:04:36 +0500

    feat(projects): add use-query for projects

--- lib/fetch-fns/fetch-projects.ts
+++ lib/fetch-fns/fetch-projects.ts
@@ -1,6 +1,7 @@
 import { apiRoutes } from '@/lib/routes/api-routes';
 import { ProjectListDTO } from '@/types/prisma/DTO/projects';
 import { AppError } from '../errors';
+import { useQuery } from '@tanstack/react-query';
 
 export const fetchProjects = async (
   workspaceId: number
@@ -34,4 +35,27 @@ export const fetchProjects = async (
       'Ошибка при получении проектов'
     );
   }
+};
+
+export const useProjects = (workspaceId: number) => {
+  return useQuery({
+    queryKey: ['projects', workspaceId], // workspaceId - добавляем что бы при изменении workspace запрос обновлялся
+    queryFn: async () => {
+      const res = await fetch(apiRoutes.getProjects(workspaceId), {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' },
+      });
+
+      if (!res.ok) {
+        throw new AppError(
+          500,
+          'FETCH_PROJECTS_ERROR',
+          'Ошибка при получении проектов'
+        );
+      }
+
+      const data = await res.json();
+      return data.data as ProjectListDTO[];
+    },
+  });
 };
