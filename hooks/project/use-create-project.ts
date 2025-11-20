e02ed790dd463a4a396210b982202c599204fb6b
Nursultan Elshatuly <sultanelshatuly@gmail.com>
2025-11-20 09:05:22 +0500

    add use mutation to creating project and using them in form

---
+++ hooks/project/use-create-project.ts
@@ -0,0 +1,21 @@
+import { apiRoutes } from '@/lib/routes/api-routes';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+
+export const useCreateProject = (workspaceId: number) => {
+  const qc = useQueryClient();
+
+  return useMutation({
+    mutationFn: async (payload: unknown) => {
+      const res = await fetch(apiRoutes.createProject(workspaceId), {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(payload),
+      });
+
+      return await res.json();
+    },
+    onSuccess: () => {
+      qc.invalidateQueries({ queryKey: ['projects', workspaceId] });
+    },
+  });
+};
