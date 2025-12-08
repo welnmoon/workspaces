7ea4d402613b68a96140676a099e0f402fa53acb
Nursultan Elshatuly <sultanelshatuly@gmail.com>
2025-12-08 16:15:09 +0500

    feat(checkout): delete unused page

---
+++ components/root/main/faq/faq.tsx
@@ -0,0 +1,17 @@
+import { Heading } from '@/components/ui/heading';
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion"
+
+const FAQ = () => {
+  return (
+    <section>
+      <Heading level={3}>FAQ</Heading>
+    </section>
+  );
+};
+
+export default FAQ;
