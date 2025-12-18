f3feceec2912d13a415015c6ed78796645ef678b
Nursultan Elshatuly <sultanelshatuly@gmail.com>
2025-12-18 08:41:21 +0500

    feat(profile) перенес профиль в dashboard

---
+++ app/(dashboard)/profile/page.tsx
@@ -0,0 +1,31 @@
+import { Breadcrumbs } from '@/components/bread-crumbs';
+import ProfileComponent from '@/components/profile/profile';
+import { requireUser } from '@/helpers/require-user';
+import { clientRoutes } from '@/lib/routes/client-routes';
+import { UserService } from '@/lib/services/user';
+import { redirect } from 'next/navigation';
+
+const ProfilePage = async () => {
+  try {
+    const { id } = await requireUser();
+    const user = await UserService.getUserProfile(id);
+
+    if (!id || !user) redirect(process.env.NEXT_PUBLIC_BASE_URL!);
+
+    return (
+      <main className="flex flex-col gap-4">
+        <Breadcrumbs
+          items={[
+            { label: 'Workspaces', href: clientRoutes.workspacesPage() },
+            { label: 'Profile' },
+          ]}
+        />
+        <ProfileComponent userId={user.id} />
+      </main>
+    );
+  } catch {
+    redirect(process.env.NEXT_PUBLIC_BASE_URL!);
+  }
+};
+
+export default ProfilePage;
