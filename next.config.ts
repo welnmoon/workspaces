e85eec653e566f116f7c41affcc1612479163018
Nursultan Elshatuly <sultanelshatuly@gmail.com>
2026-01-16 19:44:24 +0500

    fix: rewrite to SPA

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,15 @@
 const nextConfig = {
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true }, // TODO: временно - Удалить после стабилизации проекта
+  async rewrites() {
+    return {
+      afterFiles: [
+        {
+          source: '/spa/:path*',
+          destination: '/spa/index.html',
+        },
+      ],
+    };
+  },
 };
 module.exports = nextConfig;
