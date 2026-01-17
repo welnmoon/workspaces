6c56092cc8a94c7fcd6fee5266c647b2cd1ff4f2
Nursultan Elshatuly <sultanelshatuly@gmail.com>
2026-01-17 09:28:00 +0500

    feat: Разделил Next App и SPA на два разных проекта в Vercel. Delete npm run build:spa from scripts

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,22 @@
+// next.config.ts
+import type { NextConfig } from 'next';
+
 const nextConfig = {
+  productionBrowserSourceMaps: false,
+
+  webpack(config) {
+    config.devtool = false;
+    return config;
+  },
+
   eslint: { ignoreDuringBuilds: true },
-  typescript: { ignoreBuildErrors: true }, // TODO: временно - Удалить после стабилизации проекта
+  typescript: { ignoreBuildErrors: true },
+
   async rewrites() {
     return {
-      afterFiles: [
-        {
-          source: '/spa/:path*',
-          destination: '/spa/index.html',
-        },
-      ],
+      afterFiles: [{ source: '/spa/:path*', destination: '/spa/index.html' }],
     };
   },
-};
-module.exports = nextConfig;
+} satisfies NextConfig;
+
+export default nextConfig;
