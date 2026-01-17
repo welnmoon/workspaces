bec0d0f0c33e08e54671d1f975f86b430184eb98
Nursultan Elshatuly <sultanelshatuly@gmail.com>
2026-01-17 07:41:03 +0500

    fix: CORS error

--- spa/src/app/store/api.ts
+++ spa/src/app/store/api.ts
@@ -3,9 +3,11 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const api = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({
-    baseUrl:
-      `${import.meta.env.VITE_API_ORIGIN}/api/spa` ||
-      'http://workspaces-phi.vercel.app/api',
+    baseUrl: import.meta.env.PROD
+      ? '/api/spa'
+      : import.meta.env.VITE_API_ORIGIN
+        ? `${import.meta.env.VITE_API_ORIGIN}/api/spa`
+        : '/api/spa',
 
     credentials: 'include',
   }),
